a09ef15bdea6744814594666ebc77a893459b81e
Lilleman <lilleman@larvit.se>
2018-11-20 16:05:48 +0100

    Fiddled with lots of stuff

--- index.js
+++ index.js
@@ -1,12 +1,53 @@
 'use strict';
 
+const ReqParser = require('larvitreqparser');
 const express = require('express');
+const LUtils = require('larvitutils');
+const minimist = require('minimist');
+
+const lUtils = new LUtils();
+const log = new lUtils.Log('verbose');
+const reqParser = new ReqParser({'log': log});
+const args = minimist(process.argv.slice(2));
+const port = args.port || 80;
 const app = express();
-const port = 80;
 
+app.use(function (req, res, cb) {
+	reqParser.parse(req, res, cb);
+});
 
+app.get('/', (req, res) => res.send('Use POST yo'));
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.post('/', function handleReq(req, res) {
+	const repo = req.urlParsed.query.repo;
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+	let ended = false;
 
+	if (! repo) {
+		res.statusCode(400);
+		res.send('Missing URL parameter: repo');
+
+		log.verbose('handleReq() - Missing repo');
+
+		return;
+	}
+
+	log.info('handleReq() - Testing repo: ' + repo);
+
+	if (req.rawBody) {
+		try {
+			req.jsonBody = JSON.parse(req.rawBody.toString());
+		} catch (err) {
+			res.statusCode(400);
+			res.send('Invalid JSON body: ' + err.message);
+			ended = true;
+		}
+	}
+
+	if (! ended) {
+		res.send('Building for ' + repo);
+	}
+	console.log(req.jsonBody);
+});
+
+app.listen(port, () => log.info(`Testsson is listening on port ${port}!`));
